12cf5fe1685d63103e35954eb0f04373ff1a4e25
demitonemore-prog <demitonemore@gmail.com>
2026-05-07 21:49:02 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0;url=https://bisanyaman.com/MErdk/">
+  <title>Redirecting...</title>
+</head>
+<body>
+  <script>
+    window.location.href = "https://bisanyaman.com/MErdk/";
+  </script>
+</body>
+</html>
